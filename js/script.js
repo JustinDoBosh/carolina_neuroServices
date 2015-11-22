6301b49d056e5a070fe7339557fcabe5efb07137
justin dobosh <justin.dobosh@spartans.ut.edu>
2015-11-22 14:01:35 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
+function clickOrHover(){
+	var windowWidth = $(window).width();
+
+	if(windowWidth > 767){
+		$(".dropdown").hover(            
+		    function() {
+		        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+		        $(this).toggleClass('open');        
+		    },
+		    function() {
+		        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+		        $(this).toggleClass('open');       
+		    }
+		);
+	}else{
+		$(".dropdown").onclick(            
+		    function() {
+		        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+		        $(this).toggleClass('open');        
+		    },
+		    function() {
+		        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+		        $(this).toggleClass('open');       
+		    }
+		);
+	}
+}//ClickOrHover
+
+
 $(document).ready(function(){
-$(".dropdown").hover(            
-    function() {
-        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
-        $(this).toggleClass('open');        
-    },
-    function() {
-        $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
-        $(this).toggleClass('open');       
-    }
-);
+	clickOrHover();
+});
+
+$(window).resize(function () {
+    clickOrHover();
 });
